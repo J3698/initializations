2c6d1ebd4ed1cfae7eb5bb71532c9cb6d75da03a
Antioch Sanders <antiochsanders@gmail.com>
2021-03-15 03:09:07 -0400

    Add LSUV helper functions for use in PCA/ZCA init

---
+++ initializers/lsuv.py
@@ -0,0 +1,36 @@
+from initializers.common import *
+
+
+def iteratively_scale_and_rebias_layer(layer, batches, max_iters = 5,
+                                      verbose = False):
+    """
+        Iteratively scale weights and set bias so output var is 1, mean is 0.
+        Stops when var / mean are close enough to 1 / 0 respectively, or max
+        iterations have been reached
+    """
+
+    channel_means, channel_vars = calc_channel_means_and_vars(layer, batches)
+    avg_squared_mean, avg_var = calc_avg_squared_mean_and_avg_var(channel_means, channel_vars)
+
+    max_iters_left = max_iters
+
+    while max_iters_left > 0 and (avg_squared_mean > 1e-10 or abs(avg_var - 1) > 1e-10):
+        scale_and_rebias_layer(layer, channel_means, channel_vars)
+
+        channel_means, channel_vars = calc_channel_means_and_vars(layer, batches)
+        avg_squared_mean, avg_var = calc_avg_squared_mean_and_avg_var(channel_means, channel_vars)
+
+        max_iters_left -= 1
+
+        if verbose:
+            print(f"avg squared mean: {avg_squared_mean}")
+            print(f"avg var: {avg_var}")
+
+def scale_and_rebias_layer(layer, channel_means, channel_vars):
+    """
+        Scale weights and set bias so output var is 1, mean is 0
+    """
+
+    with torch.no_grad():
+        layer.weight /= channel_vars[:, None, None, None] ** 0.5
+        layer.bias -= channel_means
